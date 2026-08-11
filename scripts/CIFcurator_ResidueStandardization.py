8d5c7a560858893e3fd80c5124a1a014a7369217
Paulina Cabral <137828909+gdkwxn@users.noreply.github.com>
2026-08-11 12:44:53 -0700

    Change import path for pdbcleanresiduestandardizationutils

--- scripts/CIFcurator_ResidueStandardization.py
+++ scripts/CIFcurator_ResidueStandardization.py
@@ -4,7 +4,7 @@
 from __future__ import print_function
 from __future__ import division
 import sys, glob
-from PDBClean import pdbcleanresiduestandardizationutils as resstd
+from CIFcurator import pdbcleanresiduestandardizationutils as resstd
 
 ########################
 # READ INPUT ARGUMENTS #
