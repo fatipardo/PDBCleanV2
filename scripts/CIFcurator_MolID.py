b0496db7aaa072698a4b3349be4c4cfd085d0277
Paulina Cabral <137828909+gdkwxn@users.noreply.github.com>
2026-08-11 12:44:20 -0700

    Update import path for pdbcleanmolidcifutils

--- scripts/CIFcurator_MolID.py
+++ scripts/CIFcurator_MolID.py
@@ -10,7 +10,7 @@
 
 from __future__ import print_function
 import sys, glob
-from PDBClean import pdbcleanmolidcifutils as molidutils
+from CIFcurator import pdbcleanmolidcifutils as molidutils
 
 
 ########################
