aaf5e8eda890bc0b6d09b7afdfd731586aaae25d
Paulina Cabral <137828909+gdkwxn@users.noreply.github.com>
2026-08-12 14:18:18 -0700

    changed any mention of pdbclean to cifcurator

--- scripts/CIFcurator_ResidueStandardization.py
+++ scripts/CIFcurator_ResidueStandardization.py
@@ -4,7 +4,7 @@
 from __future__ import print_function
 from __future__ import division
 import sys, glob
-from CIFcurator import pdbcleanresiduestandardizationutils as resstd
+from CIFcurator import cifcuratorresiduestandardizationutils as resstd
 
 ########################
 # READ INPUT ARGUMENTS #
