30b8bdfafeaf6c74d43ad2dbfa456ffaf3b0f3f1
Paulina Cabral <137828909+gdkwxn@users.noreply.github.com>
2026-08-12 14:17:36 -0700

    changed any mention of pdbclean to cifcurator

--- scripts/CIFcurator_MolID.py
+++ scripts/CIFcurator_MolID.py
@@ -10,7 +10,7 @@
 
 from __future__ import print_function
 import sys, glob
-from CIFcurator import pdbcleanmolidcifutils as molidutils
+from CIFcurator import cifcuratormolidcifutils as molidutils
 
 
 ########################
